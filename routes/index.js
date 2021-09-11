a7fb5a078b6a81d0596e6ebd0e33d36128d6affa
donghyuk <gohyuk528@gmail.com>
2021-09-11 12:06:09 +0900

    Feat: add new routes for creating a prescription

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { verifyToken } = require("../middlewares/verifyToken");
 const loginController = require("./controllers/login.controller");
+const medicineController = require("./controllers/medicine.controller");
 const serviceController = require("./controllers/service.controller");
 const prescriptionController = require("./controllers/prescription.controller");
 
@@ -11,11 +12,15 @@ router.post("/logout", loginController.logout);
 router.get("/auth-check", verifyToken, loginController.authorize);
 
 router.get("/prescriptions", verifyToken, prescriptionController.getPrescriptionList);
+// router.get("/prescriptions/?offset=0?limit=8", verifyToken, prescriptionController.getPaginated);
 router.get(
   "/prescriptions/:prescriptionId",
   verifyToken,
   prescriptionController.getPrescriptionDetails
 );
+
+router.post("/prescription", verifyToken, prescriptionController.postPrescription);
+router.post("/medicine", verifyToken, medicineController.getMedicineDetails);
 
 router.get("/queue", verifyToken, serviceController.getQueue);
 router.post("/queue", verifyToken, serviceController.registerQueue);
