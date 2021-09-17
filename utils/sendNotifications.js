52a35bd664b4aeb7a3417d23d4ef3efac8c6bf9a
donghyuk <gohyuk528@gmail.com>
2021-09-17 20:26:31 +0900

    Feat: fetch to notification server

---
+++ utils/sendNotifications.js
@@ -0,0 +1,21 @@
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const fetchToNotificationServer = async (information) => {
+  const response = await fetch(`${process.env.PUSH_NOTIFICATION_URI}/notification`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+
+    body: JSON.stringify(information),
+  });
+
+  if (!response.ok) {
+    throw Error("Interner Server Error");
+  }
+
+  return await response.json();
+};
+
+module.exports = fetchToNotificationServer;
